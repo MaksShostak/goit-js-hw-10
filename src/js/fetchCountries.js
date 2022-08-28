948a0f2e88871e2cd326921ecbb81e3692b0df4b
MaksShostak <107349913+MaksShostak@users.noreply.github.com>
2022-08-28 20:58:19 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
 function fetchCountries(countrytName) {
   const url = `https://restcountries.com/v3.1/name/${countrytName}?fields=name,capital,population,flags,languages`;
-  return fetch(url)
-    .then(response => response.json())
-    .catch(error => Notify.failure(error));
+  return fetch(url).then(response => response.json());
 }
 export { fetchCountries };
